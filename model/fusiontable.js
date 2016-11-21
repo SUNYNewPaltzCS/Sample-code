15c4a706e52896dc0955c5cc740f0b228c0fa992
Rob <wielandr1@gmail.com>
2016-11-21 15:51:06 -0500

    Added comments about OAuth

--- model/fusiontable.js
+++ model/fusiontable.js
@@ -14,21 +14,25 @@
 
 'use strict';
 
-var google = require('googleapis');
-var fs = require("fs");
-var OAuth2Client = google.auth.OAuth2;
-var ft = google.fusiontables('v2');
+var google = require('googleapis');   // all api access from google
+var fs = require("fs");               // File System allows us to read in client_secrets.json
+var OAuth2Client = google.auth.OAuth2; // retrieves OAuth2 class
+var ft = google.fusiontables('v2');    // retrieves class. 'V2' = version 2 of fusion tables
 
 // Client ID and client secret are available at
 // https://code.google.com/apis/console
 var client_secrets = JSON.parse(fs.readFileSync('/home/ubuntu/workspace/client_secrets.json'));
 var CLIENT_ID = client_secrets.web.client_id;
 var CLIENT_SECRET = client_secrets.web.client_secret;
+
+// location of the page google will return the user to after once client is
+// logged into their google account and allowed permissions.
 var REDIRECT_URL = 'https://builder2-deisingj1.c9users.io/fusiontable/auth';
 
+//
 var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 google.options({
-    auth: oauth2Client
+    auth: oauth2Client  // sets this OAuth2Client as the authentication method
 });
 
 module.exports = {
@@ -54,7 +58,12 @@ module.exports = {
             }
             // set tokens to the client
             // TODO: tokens should be set by OAuth2 client.
+
+            // sets the tokens that have been received to
+            // the credentials of the client.
             oauth2Client.setCredentials(tokens);
+
+            // retreives all fusion tables the user owns.
             ft.table.list({}, [], function(err, profile) {
                 if (err) {
                     console.log('An error occured', err);
